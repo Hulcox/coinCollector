5ccd3eafa662497af0df5a4b4fca677c0749e97d
Hulcox <hulcox03@gmail.com>
2024-05-22 11:36:52 +0200

    testing react native

---
+++ app/(tabs)/test.tsx
@@ -0,0 +1,29 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Image, Platform } from "react-native";
+
+const TestPage = () => {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+      }
+    ></ParallaxScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
+
+export default TestPage;
