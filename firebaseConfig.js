94db6655b76bc332e66a892b9407281905b81beb
geoffreyrig4l <rigalgeoffrey29@gmail.com>
2024-05-23 18:33:09 +0200

    change firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
@@ -24,6 +23,5 @@ const app = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
 export const db = getFirestore(app);
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
+
+export const auth = initializeAuth(app);
